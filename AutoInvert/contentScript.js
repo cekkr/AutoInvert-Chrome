94efa4b9f197d915111926721d46339dbd17d4e3
Riccardo Cecchini <rcecchini.ds@gmail.com>
2022-09-29 02:12:52 +0200

    Improved link readability

--- AutoInvert/contentScript.js
+++ AutoInvert/contentScript.js
@@ -205,7 +205,7 @@ function getInvertStyle(invert){
   // background-color: white;
 
   let imgExcludeContrastFilter = invert ? 'contrast(1.1); border-radius: 5px;' : ''; // this compensate some website visualization problem (removed 'contrast(0.80) brightness(1.10)')
-  let bodyTextShadow = invert ? 'body{text-shadow: 0px 0px 1px rgba(0, 0, 0, 1);} a{ color: #031d38; }' : ''; // removed: text-shadow: 0px 0px 1px rgba(127, 127, 127, 1);
+  let bodyTextShadow = invert ? 'body{text-shadow: 0px 0px 1px rgba(0, 0, 0, 1);} a{ /*color: #031d38;*/ -webkit-text-stroke: 0.25px black; }' : ''; // removed: text-shadow: 0px 0px 1px rgba(127, 127, 127, 1);
 
   let style = `
   html { 
